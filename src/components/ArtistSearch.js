45b4dd1a506d85174ee0cb27b7de602b94c26c21
veranyc <veranyc@gmail.com>
2017-08-13 09:29:21 -0400

    got api to connect

--- src/components/ArtistSearch.js
+++ src/components/ArtistSearch.js
@@ -1,11 +1,34 @@
  import React from 'react'
+ const getArtistSearchUrl = 'https://api.setlist.fm/rest/0.1/search/artists?artistName=lcd+soundsystem'
 
  export default class ArtistSearch extends React.Component{
     constructor(){
       super()
+      this.state = {
+        artistResults: null,
+        selectedArtist: null
+      }
     }
 
+    componentDidMount() {
+    fetch(getArtistSearchUrl, {
+        method: 'get',
+        headers: {
+          "x-api-key": "31fb9db3-0d0f-493c-8b73-8b824360134f",
+          'Accept': 'application/json'
+        }
+    })
+      .then(response => response.json())
+      .then(responseData => {
+        this.setState({artistResults: responseData.artists.artist})
+      })
+      .catch(error => {
+        console.log('Error in fetching data', error)
+      })
+  }
+
    render(){
+      console.log(this.state.artistResults)
       return(
         <div>
           <p>Search for an Artist</p>
